75945017037cec024515e54b3c2b63467760f504
dragan <dragang@iborn.net>
2019-01-11 17:10:27 +0100

    Exercises changes

--- Class2/Exercises/Exercise 1/script.js
+++ Class2/Exercises/Exercise 1/script.js
@@ -1,81 +1,24 @@
-// // Bad way
-// $(document).ready(function(){
-//     let callBtn = document.getElementById("callBtn");
-//     let result = document.getElementById("result");
-//     let globalResponse;
-//     callBtn.addEventListener("click", function(){
-//         $.ajax({
-//             url:"https://raw.githubusercontent.com/Drakso/JavaScriptAjax/master/students6.json",
-//             success: function(response){
-//                 console.log("Success!");
-//                 globalResponse = JSON.parse(response);
-//             },
-//             error: function(error){
-//                 console.log(error);
-//             }
-//         })
-//         let resultTitle = result.getElementsByTagName("h1")[0];
-//         let resultList = result.getElementsByTagName("ul")[0];
-//         resultTitle.innerText = `${globalResponse.Academy} Academy:`
-//         resultList.innerHTML = "";
-//         for (let student of globalResponse.students) {
-//             resultList.innerHTML += `<li>${student}</li>`
-//         }
-//     })
-// })
+function isOddEven(number){
+  if (num % 2 === 0) {
+    stats.isEven = true;
+  } else {
+    stats.isEven = false;
+  }
+}
 
-// Okay way
-$(document).ready(function(){
-    let callBtn = document.getElementById("callBtn");
-    let result = document.getElementById("result");
-    callBtn.addEventListener("click", function(){
-        $.ajax({
-            url:"https://raw.githubusercontent.com/Drakso/JavaScriptAjax/master/students6.json",
-            success: function(response){
-                console.log("Success!");
-                let responseObject = JSON.parse(response);
-                let resultTitle = result.getElementsByTagName("h1")[0];
-                let resultList = result.getElementsByTagName("ul")[0];
-                resultTitle.innerText = `${responseObject.Academy} Academy:`
-                resultList.innerHTML = "";
-                for (let student of responseObject.students) {
-                    resultList.innerHTML += `<li>${student}</li>`
-                }
-            },
-            error: function(error){
-                console.log(error);
-            }
-        })
-    })
-})
+function isPositiveNegative(number){
+  if (number >= 0) {
+    return "Positive";
+  } else {
+    return "Negative";
+  }
 
-// // Good way
-// function printAcademy(resultElement, academy){
-//     let resultTitle = resultElement.getElementsByTagName("h1")[0];
-//     resultTitle.innerText = `${academy} Academy:`
-// }
-// function printStudents(resultElement, students){
-//     let resultList = resultElement.getElementsByTagName("ul")[0];
-//     resultList.innerHTML = "";
-//     for (let student of students) {
-//         resultList.innerHTML += `<li>${student}</li>`
-//     }
-// }
-// $(document).ready(function(){
-//     let callBtn = document.getElementById("callBtn");
-//     let result = document.getElementById("result");
-//     callBtn.addEventListener("click", function(){
-//         $.ajax({
-//             url:"https://raw.githubusercontent.com/Drakso/JavaScriptAjax/master/students6.json",
-//             success: function(response){
-//                 console.log("Success!");
-//                 let responseObject = JSON.parse(response);
-//                 printAcademy(result, responseObject.Academy);
-//                 printStudents(result, responseObject.students);
-//             },
-//             error: function(error){
-//                 console.log(error);
-//             }
-//         })
-//     })
-// })
+}
+
+function getDigits(number){
+  if(number >= 0) {
+    return number.toString().length;
+  } else {
+    return number.toString().length - 1;
+  }
+}
