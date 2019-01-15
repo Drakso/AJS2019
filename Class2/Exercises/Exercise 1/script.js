e0f430703bc4116599ed5f4909e0c2b9589cbfa2
dragan <dragang@iborn.net>
2019-01-15 16:35:11 +0100

    Class 3 readme

--- Class2/Exercises/Exercise 1/script.js
+++ Class2/Exercises/Exercise 1/script.js
@@ -1,24 +1,28 @@
-function isOddEven(number){
-  if (num % 2 === 0) {
-    stats.isEven = true;
-  } else {
-    stats.isEven = false;
-  }
-}
-
-function isPositiveNegative(number){
-  if (number >= 0) {
-    return "Positive";
-  } else {
-    return "Negative";
-  }
-
-}
-
+// Exercise 1
 function getDigits(number){
-  if(number >= 0) {
-    return number.toString().length;
-  } else {
-    return number.toString().length - 1;
+  if(number < 0){
+    return number.toString().length -1;
   }
+  return number.toString().length;
 }
+
+function oddOrEven(number){
+  return number % 2 === 0 ? "even" : "odd";
+}
+
+function positiveOrNegative(number){
+  return number < 0 ? "negative" : "positive";
+}
+
+function getStats(mainNumber){
+  return `Number: ${mainNumber},
+  Digits: ${getDigits(mainNumber)}, 
+  ${oddOrEven(mainNumber)}, 
+  ${positiveOrNegative(mainNumber)}`
+}
+
+console.log(getStats(25));
+console.log(getStats(-252));
+console.log(getStats(3));
+console.log(getStats(0));
+console.log(getStats(-7));
